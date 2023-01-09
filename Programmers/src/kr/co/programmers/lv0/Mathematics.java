b49b7917f90b5cc60d944325e923fcb460280630
Kyle <109461345+Engineering-Excellence@users.noreply.github.com>
2023-01-09 10:47:20 +0900

    사칙연산, 배열, 수학 - 나머지 구하기

---
+++ Programmers/src/kr/co/programmers/lv0/Mathematics.java
@@ -0,0 +1,27 @@
+package kr.co.programmers.lv0;
+
+import java.util.*;
+
+// 사칙연산, 배열, 수학
+public class Mathematics {
+
+    // 나머지 구하기
+    private int mod(int num1, int num2) {
+
+        return num1 % num2;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("첫 번째 숫자를 입력하세요.: ");
+        int num1 = scanner.nextInt();
+        System.out.println("두 번째 숫자를 입력하세요.: ");
+        int num2 = scanner.nextInt();
+        scanner.close();
+
+        Mathematics mathematics = new Mathematics();
+        System.out.printf("%d %% %d = %d%n", num1, num2, mathematics.mod(num1, num2));
+
+    }
+}
