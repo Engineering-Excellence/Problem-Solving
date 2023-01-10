9aad6e832d0672ef741714185b9efaa3e69ccc53
Kyle <109461345+Engineering-Excellence@users.noreply.github.com>
2023-01-10 09:52:17 +0900

    사칙연산, 배열, 수학 - 중앙값 구하기

--- Programmers/src/kr/co/programmers/lv0/Mathematics.java
+++ Programmers/src/kr/co/programmers/lv0/Mathematics.java
@@ -11,6 +11,13 @@ public class Mathematics {
         return num1 % num2;
     }
 
+    // 중앙값 구하기
+    private int median(int[] array) {
+
+        Arrays.sort(array);
+        return array[array.length / 2];
+    }
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -22,6 +29,6 @@ public class Mathematics {
 
         Mathematics mathematics = new Mathematics();
         System.out.printf("%d %% %d = %d%n", num1, num2, mathematics.mod(num1, num2));
-
+        System.out.println(mathematics.median(new int[]{9, -1, 0}));
     }
 }
