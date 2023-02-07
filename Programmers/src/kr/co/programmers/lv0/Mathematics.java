d288fde535bb048320da2eff3091bbc7c1e788d5
Kyle <prophet@hanyang.ac.kr>
2023-02-07 19:35:02 +0900

    사칙연산, 배열, 수학 - 최빈값 구하기

--- Programmers/src/kr/co/programmers/lv0/Mathematics.java
+++ Programmers/src/kr/co/programmers/lv0/Mathematics.java
@@ -18,6 +18,25 @@ public class Mathematics {
         return array[array.length / 2];
     }
 
+    // 최빈값 구하기
+    private int mode(int[] array) {
+
+        int maxCount = 0;
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int number : array) {
+            int count = map.getOrDefault(number, 0) + 1;
+            if (count > maxCount) {
+                maxCount = count;
+                answer = number;
+            } else if (count == maxCount) {
+                answer = -1;
+            }
+            map.put(number, count);
+        }
+        return answer;
+    }
+
     // 짝수는 싫어요
     private int[] odd(int n) {
 
@@ -40,6 +59,10 @@ public class Mathematics {
         Mathematics mathematics = new Mathematics();
         System.out.printf("%d %% %d = %d%n", num1, num2, mathematics.mod(num1, num2));
         System.out.println(mathematics.median(new int[]{9, -1, 0}));
+        System.out.println(mathematics.mode(new int[]{1, 2, 3, 3, 3, 4}));
+        System.out.println(mathematics.mode(new int[]{1, 2, 2, 1}));
+        System.out.println(mathematics.mode(new int[]{1}));
+        System.out.println(mathematics.mode(new int[]{1, 2, 3, 3, 3, 4, 5, 5, 5, 5}));
         System.out.println(Arrays.toString(mathematics.odd(10)));
         System.out.println(Arrays.toString(mathematics.odd(15)));
     }
