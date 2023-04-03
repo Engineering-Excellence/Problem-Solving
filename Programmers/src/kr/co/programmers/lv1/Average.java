55aa67f82f2b8203f1a7b508ffe9101c3972c2ba
Kyle <prophet@hanyang.ac.kr>
2023-04-03 23:40:12 +0900

    Lv.1 평균 구하기

---
+++ Programmers/src/kr/co/programmers/lv1/Average.java
@@ -0,0 +1,19 @@
+package kr.co.programmers.lv1;
+
+import java.util.Arrays;
+
+// 평균 구하기
+public class Average {
+
+    private double solution(int[] arr) {
+
+        return Arrays.stream(arr).average().getAsDouble();
+    }
+
+    public static void main(String[] args) {
+
+        Average average = new Average();
+        System.out.println(average.solution(new int[]{1, 2, 3, 4}));
+        System.out.println(average.solution(new int[]{5, 5}));
+    }
+}
