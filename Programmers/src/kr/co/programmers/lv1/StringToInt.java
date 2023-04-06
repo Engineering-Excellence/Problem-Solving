bd0c180fb0b6b87a49330c5c647ccc80a0d05c85
Kyle <prophet@hanyang.ac.kr>
2023-04-07 00:00:10 +0900

    Lv.1 문자열을 정수로 바꾸기

---
+++ Programmers/src/kr/co/programmers/lv1/StringToInt.java
@@ -0,0 +1,18 @@
+package kr.co.programmers.lv1;
+
+// 문자열을 정수로 바꾸기
+
+public class StringToInt {
+
+    private int solution(String s) {
+
+        return Integer.parseInt(s);
+    }
+
+    public static void main(String[] args) {
+
+        StringToInt stringToInt = new StringToInt();
+
+        System.out.println(stringToInt.solution("-1234"));
+    }
+}
