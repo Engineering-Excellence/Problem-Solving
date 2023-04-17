486477dc5990f88df2555e526420cccc88522d26
Kyle <prophet@hanyang.ac.kr>
2023-04-17 17:36:03 +0900

    Lv.1 정수 제곱근 판별

---
+++ Programmers/src/kr/co/programmers/lv1/DetSqrt.java
@@ -0,0 +1,18 @@
+package kr.co.programmers.lv1;
+
+// 정수 제곱근 판별
+public class DetSqrt {
+
+    private long solution(long n) {
+
+        return n % Math.sqrt(n) == 0 ? (long) Math.pow(Math.sqrt(n) + 1, 2) : -1;
+    }
+
+    public static void main(String[] args) {
+
+        DetSqrt detSqrt = new DetSqrt();
+
+        System.out.println(detSqrt.solution(121));
+        System.out.println(detSqrt.solution(3));
+    }
+}
