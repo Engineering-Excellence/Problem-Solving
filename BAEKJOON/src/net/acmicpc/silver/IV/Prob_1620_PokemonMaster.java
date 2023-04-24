6a1762434876ba3c19f08ffd60765ce2a54db044
Kyle <prophet@hanyang.ac.kr>
2023-04-24 17:21:28 +0900

    BAEKJOON 1620 - 나는야 포켓몬 마스터 이다솜

---
+++ BAEKJOON/src/net/acmicpc/silver/IV/Prob_1620_PokemonMaster.java
@@ -0,0 +1,39 @@
+package net.acmicpc.silver.IV;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+// 나는야 포켓몬 마스터 이다솜
+public class Prob_1620_PokemonMaster {
+
+    public static void main(String[] args) throws IOException {
+
+//        System.setIn(new FileInputStream("BAEKJOON/sample_input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] numbers = br.readLine().split(" ");
+        int N = Integer.parseInt(numbers[0]);
+        int M = Integer.parseInt(numbers[1]);
+
+        StringBuilder ans = new StringBuilder();
+        Map<Integer, String> pokemonMap = new HashMap<>();
+        Map<String, Integer> reverseMap = new HashMap<>();
+
+        for (int i = 1; i <= N; i++) {
+            String pokemon = br.readLine();
+            pokemonMap.put(i, pokemon);
+            reverseMap.put(pokemon, i);
+        }
+        for (int i = 1; i <= M; i++) {
+            String prob = br.readLine();
+            try {
+                ans.append(pokemonMap.get(Integer.parseInt(prob))).append("\n");
+            } catch (NumberFormatException e) {
+                ans.append(reverseMap.get(prob)).append("\n");
+            }
+        }
+        System.out.print(ans);
+    }
+}
