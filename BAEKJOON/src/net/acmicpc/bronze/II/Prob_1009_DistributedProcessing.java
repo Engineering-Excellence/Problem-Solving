771c19f66cfd7c6b56b0716c92b4c48c396c0198
Kyle <prophet@hanyang.ac.kr>
2023-04-24 17:22:51 +0900

    BAEKJOON 1009 - 분산처리

---
+++ BAEKJOON/src/net/acmicpc/bronze/II/Prob_1009_DistributedProcessing.java
@@ -0,0 +1,35 @@
+package net.acmicpc.bronze.II;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+// 분산처리
+public class Prob_1009_DistributedProcessing {
+
+    static int a, b, res;
+
+    public static void main(String[] args) throws Exception {
+
+        System.setIn(new FileInputStream("BAEKJOON/sample_input.txt"));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+
+            String[] input = br.readLine().split(" ");
+            a = Integer.parseInt(input[0]);
+            b = Integer.parseInt(input[1]);
+            res = a % 10;
+
+            for (int i = 1; i < b; i++) {
+                res = (res * (a % 10)) % 10;
+            }
+            ans.append(res != 0 ? res : 10).append("\n");
+        }
+        System.out.print(ans);
+    }
+}
