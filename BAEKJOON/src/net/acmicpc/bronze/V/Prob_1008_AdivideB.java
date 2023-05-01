2ef101b18ab10c0d9ede783db2f8bb825030ae6e
Kyle <prophet@hanyang.ac.kr>
2023-05-01 18:10:03 +0900

    BAEKJOON 1008 - A/B

---
+++ BAEKJOON/src/net/acmicpc/bronze/V/Prob_1008_AdivideB.java
@@ -0,0 +1,16 @@
+package net.acmicpc.bronze.V;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Prob_1008_AdivideB {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        System.out.print(Double.parseDouble(st.nextToken()) / Double.parseDouble(st.nextToken()));
+    }
+}
