3b7d98fc3d0019a25a59b54fce22722a315c3f3a
Kyle <prophet@hanyang.ac.kr>
2023-05-02 09:22:03 +0900

    BAEKJOON 1157 - 단어 공부

---
+++ BAEKJOON/src/net/acmicpc/bronze/I/Prob_1157_WordStudy.java
@@ -0,0 +1,23 @@
+package net.acmicpc.bronze.I;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+// 단어 공부
+public class Prob_1157_WordStudy {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> counts = Arrays.asList(new Integer[26]);
+        Collections.fill(counts, 0);
+        br.readLine().toUpperCase().chars().forEach(c -> counts.set(c - 'A', counts.get(c - 'A') + 1));
+        int max = Collections.max(counts);
+        if (counts.indexOf(max) == counts.lastIndexOf(max)) System.out.println((char) (counts.indexOf(max) + 'A'));
+        else System.out.println("?");
+    }
+}
