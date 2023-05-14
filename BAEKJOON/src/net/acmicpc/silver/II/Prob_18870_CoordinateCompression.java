4582cb4941f35679911e63c4dcbd62f6d5ce2cb1
Kyle <prophet@hanyang.ac.kr>
2023-05-14 23:47:42 +0900

    BAEKJOON 18870 - 좌표 압축

---
+++ BAEKJOON/src/net/acmicpc/silver/II/Prob_18870_CoordinateCompression.java
@@ -0,0 +1,41 @@
+package net.acmicpc.silver.II;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+// 좌표 압축
+public class Prob_18870_CoordinateCompression {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final int N = Integer.parseInt(br.readLine());
+        int[] x = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            x[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] sorted = x.clone();
+        Arrays.sort(sorted);
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int rank = 0;
+        for (int k : sorted) {
+            if (!map.containsKey(k)) {
+                map.put(k, rank++);
+            }
+        }
+
+        StringBuilder ans = new StringBuilder();
+        for (int k : x) {
+            ans.append(map.get(k)).append(" ");
+        }
+        System.out.print(ans);
+    }
+}
