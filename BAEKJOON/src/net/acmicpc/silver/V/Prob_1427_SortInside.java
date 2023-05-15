e2c4216dcb7568ee18264abbde38396b45d01dac
Kyle <prophet@hanyang.ac.kr>
2023-05-16 04:50:17 +0900

    BAEKJOON 1427 - 소트인사이드

---
+++ BAEKJOON/src/net/acmicpc/silver/V/Prob_1427_SortInside.java
@@ -0,0 +1,17 @@
+package net.acmicpc.silver.V;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 소트인사이드
+public class Prob_1427_SortInside {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder ans = new StringBuilder();
+        br.readLine().chars().sorted().forEach(c -> ans.insert(0, (char) c));
+        System.out.print(ans);
+    }
+}
