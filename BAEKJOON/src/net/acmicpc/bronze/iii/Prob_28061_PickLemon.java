5c550e35d75a5660ed1f030339df1daf31f4235c
Kyle <prophet@hanyang.ac.kr>
2023-05-23 18:29:08 +0900

    BAEKJOON 28061 - 레몬 따기

---
+++ BAEKJOON/src/net/acmicpc/bronze/iii/Prob_28061_PickLemon.java
@@ -0,0 +1,25 @@
+package net.acmicpc.bronze.iii;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 레몬 따기
+public class Prob_28061_PickLemon {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            int temp = Integer.parseInt(st.nextToken()) - (n - i);
+            if (max < temp) {
+                max = temp;
+            }
+        }
+        System.out.print(max);
+    }
+}
