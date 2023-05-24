d90eaef89cb7e9e2d32f2fd638d05227c5bbd364
Kyle <prophet@hanyang.ac.kr>
2023-05-24 17:56:27 +0900

    BAEKJOON 2559 - 수열

---
+++ BAEKJOON/src/net/acmicpc/silver/iii/Prob_2559_Sequence.java
@@ -0,0 +1,37 @@
+package net.acmicpc.silver.iii;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 수열
+public class Prob_2559_Sequence {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        final int N = Integer.parseInt(st.nextToken());
+        final int K = Integer.parseInt(st.nextToken());
+
+        long[] celsius = new long[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            celsius[i] = Integer.parseInt(st.nextToken());
+        }
+
+        long sum = 0;
+        for (int i = 0; i < K; i++) {
+            sum += celsius[i];
+        }
+
+        long max = sum;
+        for (int i = K; i < N; i++) {
+            sum += celsius[i];
+            sum -= celsius[i - K];
+            max = Math.max(max, sum);
+        }
+        System.out.print(max);
+    }
+}
