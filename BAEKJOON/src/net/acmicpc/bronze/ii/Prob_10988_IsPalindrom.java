1e8b5f2df980a54f4242ab4ed5bc1a8c02e6deba
Kyle <prophet@hanyang.ac.kr>
2023-05-24 17:55:48 +0900

    BAEKJOON 10988 - 팰린드롬인지 확인하기

---
+++ BAEKJOON/src/net/acmicpc/bronze/ii/Prob_10988_IsPalindrom.java
@@ -0,0 +1,16 @@
+package net.acmicpc.bronze.ii;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 팰린드롬인지 확인하기
+public class Prob_10988_IsPalindrom {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder(br.readLine());
+        System.out.print(sb.toString().contentEquals(sb.reverse()) ? 1 : 0);
+    }
+}
