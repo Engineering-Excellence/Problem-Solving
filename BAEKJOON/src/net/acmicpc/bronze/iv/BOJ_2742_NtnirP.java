671c664fc1128dadb4daef759dd04c9a56c831f0
Kyle <prophet@hanyang.ac.kr>
2023-05-28 21:43:30 +0900

    BAEKJOON 2742 - 기찍 N

---
+++ BAEKJOON/src/net/acmicpc/bronze/iv/BOJ_2742_NtnirP.java
@@ -0,0 +1,19 @@
+package net.acmicpc.bronze.iv;
+
+import java.io.*;
+
+// 기찍 N
+public class BOJ_2742_NtnirP {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        final int N = Integer.parseInt(br.readLine());
+        for (int i = N; i > 0; i--) {
+            bw.write(i + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
