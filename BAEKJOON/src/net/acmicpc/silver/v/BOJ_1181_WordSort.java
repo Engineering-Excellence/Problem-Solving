706f6d4f55a984213fc2d7b6fd66c59f9fef86c0
Kyle <prophet@hanyang.ac.kr>
2023-05-31 22:58:48 +0900

    BAEKJOON 1181 - 단어 정렬

---
+++ BAEKJOON/src/net/acmicpc/silver/v/BOJ_1181_WordSort.java
@@ -0,0 +1,35 @@
+package net.acmicpc.silver.v;
+
+import java.io.*;
+import java.util.Arrays;
+
+// 단어 정렬
+public class BOJ_1181_WordSort {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        final int N = Integer.parseInt(br.readLine());
+        String[] words = new String[N];
+        for (int i = 0; i < N; i++) {
+            words[i] = br.readLine();
+        }
+        br.close();
+        Arrays.sort(words, (o1, o2) -> {
+            if (o1.length() == o2.length()) {
+                return o1.compareTo(o2);    // 사전순으로 정렬
+            } else {
+                return o1.length() - o2.length();   // 길이순으로 정렬
+            }
+        });
+        for (int i = 0; i < N; i++) {
+            if (i > 0 && words[i].equals(words[i - 1])) {
+                continue;   // 중복된 단어는 스킵
+            }
+            bw.write(words[i] + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
