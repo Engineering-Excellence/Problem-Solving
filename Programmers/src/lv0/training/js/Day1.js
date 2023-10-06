d0537e223344451ef8495ec40305d535cd0beb0f
Kyle <prophet@hanyang.ac.kr>
2023-10-06 11:20:55 +0900

    출력 - 문자열 반복해서 출력하기

--- Programmers/src/lv0/training/js/Day1.js
+++ Programmers/src/lv0/training/js/Day1.js
@@ -15,8 +15,16 @@ class Day1 {
     // a와 b 출력하기
     printSplit() {
         rl.on('line', line => {
-            const values = line.split(' ')
+            let values = line.split(' ')
             console.log(`a = ${values[0]}\nb = ${values[1]}`)
+        })
+    }
+
+    // 문자열 반복해서 출력하기
+    printRepeat() {
+        rl.on('line', line => {
+            let values = line.split(' ')
+            console.log(values[0].repeat(values[1]))
         })
     }
 }
@@ -24,3 +32,4 @@ class Day1 {
 const day1 = new Day1()
 day1.printStr()
 day1.printSplit()
+day1.printRepeat()
