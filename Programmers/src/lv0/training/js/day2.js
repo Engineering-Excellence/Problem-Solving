ef3a23fa8cb454b08e904ee1578ee8b99e2ef3d9
Kyle <prophet@hanyang.ac.kr>
2023-10-11 01:07:01 +0900

    출력, 연산 - 문자열 붙여서 출력하기

--- Programmers/src/lv0/training/js/day2.js
+++ Programmers/src/lv0/training/js/day2.js
@@ -16,7 +16,17 @@ class Day2 {
             console.log(`${a} + ${b} = ${a + b}`)
         })
     }
+
+    // 문자열 붙여서 출력하기
+    concatStr() {
+        rl.on('line', line => {
+            let strs = line.split(' ')
+            // console.log(strs[0].concat(strs[1]))
+            console.log(strs.join(''))
+        })
+    }
 }
 
 const day2 = new Day2()
 day2.printAdd()
+day2.concatStr()
