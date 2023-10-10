1a115291b746e19dda92ca50c0cebf533d4fc95e
Kyle <prophet@hanyang.ac.kr>
2023-10-11 01:47:07 +0900

    출력, 연산 - 홀짝 구분하기

--- Programmers/src/lv0/training/js/day2.js
+++ Programmers/src/lv0/training/js/day2.js
@@ -31,9 +31,15 @@ class Day2 {
         // 전개 연산자 '...': 명시적으로 할당되지 않은 나머지 배열 값
         rl.on('line', line => [...line].forEach(c => console.log(c)))
     }
+
+    // 홀짝 구분하기
+    evenOdd() {
+        rl.on('line', line => console.log(line, 'is', parseInt(line) % 2 ? 'odd' : 'even'))
+    }
 }
 
 const day2 = new Day2()
 day2.printAdd()
 day2.concatStr()
 day2.rotateStr()
+day2.evenOdd()
