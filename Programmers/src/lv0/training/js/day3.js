1d1432eca6b474af6ea55efe33b509c21d2ba876
Kyle <prophet@hanyang.ac.kr>
2023-10-11 09:24:43 +0900

    연산 - 문자 리스트를 문자열로 변환하기

--- Programmers/src/lv0/training/js/day3.js
+++ Programmers/src/lv0/training/js/day3.js
@@ -1,21 +1,22 @@
 'use strict'
 
-/* const rl = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-}) */
-
 // 연산
 class Day3 {
     // 문자열 섞기
     shuffleStr(str1, str2) {
         let str = ''
         for (let i = 0; i < str1.length; i++) {
-            str += str1[i] + str2[i]
+            str.concat(str1[i] + str2[i])
         }
         return str
+    }
+
+    // 문자 리스트를 문자열로 변환하기
+    arrToStr(arr) {
+        return arr.join('')
     }
 }
 
 const day3 = new Day3()
 console.log(day3.shuffleStr('abc', '123'))
+console.log(day3.arrToStr(['a', 'b', 'c']))
