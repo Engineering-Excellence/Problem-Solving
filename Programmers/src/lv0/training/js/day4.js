aa9a0b3f7d5c8e7bbb1be4530fffdff22f428086
Kyle <prophet@hanyang.ac.kr>
2023-11-11 00:55:08 +0900

    연산, 조건문 - 공배수

--- Programmers/src/lv0/training/js/day4.js
+++ Programmers/src/lv0/training/js/day4.js
@@ -6,8 +6,15 @@ class Day4 {
     multiple(num, n) {
         return num % n == 0 ? 1 : 0
     }
+
+    // 공배수
+    commonMultiple(number, n, m) {
+        return number % n == 0 && number % m == 0 ? 1 : 0
+    }
 }
 
 const day4 = new Day4()
 console.log(day4.multiple(98, 2))
 console.log(day4.multiple(34, 3))
+console.log(day4.commonMultiple(60, 2, 3))
+console.log(day4.commonMultiple(55, 10, 5))
