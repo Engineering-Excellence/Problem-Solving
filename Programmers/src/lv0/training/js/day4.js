d008385b228b1c4d8ca50e600e88d20df6917f45
Kyle <prophet@hanyang.ac.kr>
2023-11-12 00:59:10 +0900

    연산, 조건문 - 홀짝에 따라 다른 값 반환하기

--- Programmers/src/lv0/training/js/day4.js
+++ Programmers/src/lv0/training/js/day4.js
@@ -11,6 +11,11 @@ class Day4 {
     commonMultiple(number, n, m) {
         return number % n == 0 && number % m == 0 ? 1 : 0
     }
+
+    // 홀짝에 따라 다른 값 반환하기
+    evenOdd(n) {
+        return n % 2 == 0 ? n * (n + 1) * (n + 2) / 6 : Math.pow(Math.ceil(n / 2), 2);
+    }
 }
 
 const day4 = new Day4()
@@ -18,3 +23,5 @@ console.log(day4.multiple(98, 2))
 console.log(day4.multiple(34, 3))
 console.log(day4.commonMultiple(60, 2, 3))
 console.log(day4.commonMultiple(55, 10, 5))
+console.log(day4.evenOdd(7))
+console.log(day4.evenOdd(10))
