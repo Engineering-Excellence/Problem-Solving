d4799886678e6964ff70f41bc5537d526b3c13d7
Kyle <prophet@hanyang.ac.kr>
2023-11-13 02:16:58 +0900

    조건문 - 등차수열의 특정한 항만 더하기

--- Programmers/src/lv0/training/js/day5.js
+++ Programmers/src/lv0/training/js/day5.js
@@ -6,7 +6,6 @@ class Day5 {
     process(code) {
         let mode = 0
         let ret = ''
-
         for (let idx = 0; idx < code.length; idx++) {
             if (code[idx] === '1') {
                 mode = (mode + 1) % 2
@@ -14,11 +13,23 @@ class Day5 {
                 ret += code[idx]
             }
         }
-
         return ret.length ? ret : 'EMPTY'
+    }
+
+    // 등차수열의 특정한 항만 더하기
+    addArithmeticSequence(a, d, included) {
+        let sum = 0
+        for (let i = 0; i < included.length; i++) {
+            if (included[i]) {
+                sum += a + d * i
+            }
+        }
+        return sum
     }
 }
 
 module.exports = Day5
 console.log(Day5.prototype.process('abc1abc1abc'))
 console.log(Day5.prototype.process(''))
+console.log(Day5.prototype.addArithmeticSequence(3, 4, [true, false, false, true, true]))
+console.log(Day5.prototype.addArithmeticSequence(7, 1, [false, false, false, true, false, false, false]))
