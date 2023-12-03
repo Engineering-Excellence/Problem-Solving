b6ff68beb371d785f010a5f931a4b24d2990e82a
Kyle <prophet@hanyang.ac.kr>
2023-12-03 23:04:59 +0900

    문자열 - 문자열의 뒤의 n글자

--- Programmers/src/lv0/training/py/day9.py
+++ Programmers/src/lv0/training/py/day9.py
@@ -10,8 +10,14 @@ class Day9:
         # return ''.join(map(lambda x: my_strings[x[0]][x[1][0]:x[1][1] + 1], enumerate(parts)))
         return ''.join([my_string[part[0]:part[1] + 1] for my_string, part in zip(my_strings, parts)])
 
+    # 문자열의 뒤의 n글자
+    def get_last_n_char(self, my_string, n):
+        return my_string[-n:] if n <= len(my_string) else my_string[-len(my_string):]
+
 
 if __name__ == '__main__':
     day9 = Day9()
     print(day9.make_array_5(["0123456789", "9876543210", "9999999999999"], 50000, 5, 5))
     print(day9.make_str(["progressive", "hamburger", "hammer", "ahocorasick"], [[0, 4], [1, 2], [3, 5], [7, 7]]))
+    print(day9.get_last_n_char("ProgrammerS123", 11))
+    print(day9.get_last_n_char("He110W0r1d", 5))
