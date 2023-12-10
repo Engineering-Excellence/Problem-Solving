1ce742fb6ef89eaf37a77b224ab9c5cf337f78d0
Kyle <prophet@hanyang.ac.kr>
2023-12-11 01:21:43 +0900

    리스트(배열) - 배열 만들기 1

--- Programmers/src/lv0/training/py/day11.py
+++ Programmers/src/lv0/training/py/day11.py
@@ -8,7 +8,14 @@ class Day11:
             ans[ord(c) - (65 if c.isupper() else 71)] += 1
         return ans
 
+    # 배열 만들기 1
+    @staticmethod
+    def make_array_1(n, k):
+        return [i for i in range(k, n + 1, k)]
+
 
 if __name__ == '__main__':
     day11 = Day11()
     print(day11.count_alphabet('Programmers'))
+    print(day11.make_array_1(10, 3))
+    print(day11.make_array_1(15, 5))
