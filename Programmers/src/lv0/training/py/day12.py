5df8f5a4256ff5b4dfc9a43df41a275dd3f3c089
Kyle <prophet@hanyang.ac.kr>
2023-12-14 00:18:37 +0900

    리스트(배열) - 첫 번째로 나오는 음수

--- Programmers/src/lv0/training/py/day12.py
+++ Programmers/src/lv0/training/py/day12.py
@@ -13,8 +13,18 @@ class Day12:
             case 4:
                 return num_list[slicer[0]:slicer[1] + 1:slicer[2]]
 
+    # 첫 번째로 나오는 음수
+    @staticmethod
+    def first_negative(num_list):
+        for i in range(len(num_list)):
+            if num_list[i] < 0:
+                return i
+        return -1
+
 
 if __name__ == '__main__':
     da12 = Day12()
     print(da12.slice_list(3, [1, 5, 2], [1, 2, 3, 4, 5, 6, 7, 8, 9]))
     print(da12.slice_list(4, [1, 5, 2], [1, 2, 3, 4, 5, 6, 7, 8, 9]))
+    print(da12.first_negative([12, 4, 15, 46, 38, -2, 15]))
+    print(da12.first_negative([13, 22, 53, 24, 15, 6]))
