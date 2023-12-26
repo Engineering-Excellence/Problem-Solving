13b2c4d7ca1da5e92f7460e0c472c291ba64ac3e
Kyle <prophet@hanyang.ac.kr>
2023-12-27 00:18:11 +0900

    문자열 - 문자열이 몇 번 등장하는지 세기

--- Programmers/src/lv0/training/java/Day17.java
+++ Programmers/src/lv0/training/java/Day17.java
@@ -13,11 +13,26 @@ public class Day17 {
      * solution1 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기
      *
      * @param myString 문자열
-     * @param pat 문자열
+     * @param pat      문자열
      * @return myString의 부분 문자열중 pat로 끝나는 가장 긴 부분 문자열을 반환
      */
     private String solution1(String myString, String pat) {
         return myString.substring(0, myString.lastIndexOf(pat) + pat.length());
+    }
+
+    /**
+     * solution2 문자열이 몇 번 등장하는지 세기
+     *
+     * @param myString 문자열
+     * @param pat      문자열
+     * @return myString에서 pat이 등장하는 횟수를 반환
+     */
+    private int solution2(String myString, String pat) {
+        int cnt = 0;
+        for (int i = 0; i <= myString.length() - pat.length(); i++) {
+            if (myString.startsWith(pat, i)) cnt++;
+        }
+        return cnt;
     }
 
     public static void main(String[] args) {
@@ -25,5 +40,8 @@ public class Day17 {
 
         System.out.println(day17.solution1("AbCdEFG", "dE"));
         System.out.println(day17.solution1("AAAAaaaa", "a"));
+
+        System.out.println(day17.solution2("banana", "ana"));
+        System.out.println(day17.solution2("aaaa", "aa"));
     }
 }
