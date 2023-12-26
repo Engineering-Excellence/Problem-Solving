3848978cc9bb4eb9b0b342e7a3f091ce62e64da0
Kyle <prophet@hanyang.ac.kr>
2023-12-26 22:13:29 +0900

    [PCCE 기출문제] 1번 - 출력

---
+++ Programmers/src/lv0/pcce/java/PCCE_1.java
@@ -0,0 +1,20 @@
+package lv0.pcce.java;
+
+/**
+ * <h1>[PCCE 기출문제] 1번 / 출력</h1>
+ *
+ * @author Kyle
+ * @since 2023.12.26.
+ */
+public class PCCE_1 {
+
+    public static void main(String[] args) {
+        String msg = "Spring is beginning";
+        int val1 = 3;
+        String val2 = "3";
+
+        System.out.println(msg);
+        System.out.println(val1 + 10);
+        System.out.println(val2 + "10");
+    }
+}
