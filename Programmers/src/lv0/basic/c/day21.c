46d766a28ead38b63fe06b6b812dcc1b02530983
Kyle <prophet@hanyang.ac.kr>
2024-01-06 12:42:24 +0900

    함수(메서드) - 문자열을 정수로 변환하기

--- Programmers/src/lv0/basic/c/day21.c
+++ Programmers/src/lv0/basic/c/day21.c
@@ -6,19 +6,34 @@
  * @since 2024.01.06.
  */
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * solution 정수 부분
+ * solution3 정수 부분
+ *
  * @param flo 실수
  * @return flo의 정수 부분
  */
-int solution(double flo) {
+int solution3(double flo) {
     return (int) flo;
 }
 
+/**
+ * solution5 문자열을 정수로 변환하기
+ *
+ * @param n_str 숫자로만 이루어진 문자열
+ * @return n_str을 정수로 변환하여 반환
+ */
+int solution5(const char* n_str) {
+    return atoi(n_str);
+}
+
 int main(void) {
-    printf("%d\n", solution(1.42));
-    printf("%d\n", solution(69.32));
+    printf("%d\n", solution3(1.42));
+    printf("%d\n", solution3(69.32));
+
+    printf("%d\n", solution5("10"));
+    printf("%d\n", solution5("8542"));
 
     return 0;
 }
