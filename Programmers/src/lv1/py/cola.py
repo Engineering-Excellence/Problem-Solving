6393f851f1b68bee427a168db06d87a26197e65f
Kyle <propheta@yonsei.ac.kr>
2024-06-20 23:31:37 +0900

    콜라 문제

---
+++ Programmers/src/lv1/py/cola.py
@@ -0,0 +1,17 @@
+def solution(a: int, b: int, n: int) -> int:
+    """
+    콜라 문제
+    :param a: 콜라를 받기 위해 마트에 주어야 하는 병 수
+    :type a: int
+    :param b: 빈 병 a개를 가져다 주면 마트가 주는 콜라 병 수
+    :type b: int
+    :param n: 상빈이가 가지고 있는 빈 병의 개수
+    :type n: int
+    :return: 상빈이가 받을 수 있는 콜라의 병 수
+    :rtype: int
+    """
+    return (n - b) // (a - b) * b
+
+
+print(solution(2, 1, 20))
+print(solution(3, 1, 20))
