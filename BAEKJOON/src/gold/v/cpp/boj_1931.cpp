0cead4a35168f5618cf46837e622a260cf3543ca
Kyle <propheta@yonsei.ac.kr>
2025-06-15 21:06:00 +0900

    BAEKJOON 1931 - 회의실 배정

---
+++ BAEKJOON/src/gold/v/cpp/boj_1931.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int main() {
+    int n, end, begin;
+    vector<pair<int, int>> schedule;
+
+    cin >> n;
+
+    for (int i{0}; i < n; i++) {
+        cin >> begin >> end;
+        schedule.emplace_back(end, begin);
+    }
+
+    sort(schedule.begin(), schedule.end());
+
+    int time = schedule[0].first;
+    int count = 1;
+    for (int i = 1; i < n; i++) {
+        if (time <= schedule[i].second) {
+            count++;
+            time = schedule[i].first;
+        }
+    }
+
+    cout << count;
+}
