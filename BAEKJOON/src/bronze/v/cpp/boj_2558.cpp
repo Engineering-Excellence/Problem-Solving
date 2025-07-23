bbc897bb8807141d804fa88cd0b2cb5316d35576
Kyle <propheta@yonsei.ac.kr>
2025-07-23 15:54:18 +0900

    BAEKJOON 2558 - A+B - 2

---
+++ BAEKJOON/src/bronze/v/cpp/boj_2558.cpp
@@ -0,0 +1,25 @@
+/**
+ * A+B - 2
+ *
+ * @file boj_2558.cpp
+ * @brief A+B를 출력
+ * @author Kyle
+ * @date 2025-07-23
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int A, B;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> A >> B;
+    cout << A + B;
+
+    return 0;
+}
