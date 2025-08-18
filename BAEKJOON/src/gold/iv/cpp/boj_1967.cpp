fbee3eee8fe9601dc65257e49b95bccebe775f68
Kyle <propheta@yonsei.ac.kr>
2025-08-18 19:01:47 +0900

    BAEKJOON 1967 - 트리의 지름

---
+++ BAEKJOON/src/gold/iv/cpp/boj_1967.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n;
+vector<int> V;
+vector<pair<int, int>> node[10001];
+int maxVal = -1, maxnode = 0;
+
+void dfs(int now, int val);
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> n; //정점의 개수
+    V.resize(n + 1, 0);
+    int s, e, w;
+
+    for (int i = 0; i < n - 1; i++) {
+        cin >> s >> e >> w; //출발,도착,가중치
+        node[s].emplace_back(e, w);
+        node[e].emplace_back(s, w);
+    }
+
+    dfs(1, 0); //루트에서 가장 먼 노드를 찾음.
+    V.assign(n + 1, 0);
+    maxVal = -1;
+    dfs(maxnode, 0);
+
+    cout << maxVal << endl;
+
+    return 0;
+}
+
+void dfs(int now, int val) {
+    V[now] = 1; //방문 처리
+
+    if (maxVal < val) { //최대값이 갱신될 경우
+        maxVal = val;
+        maxnode = now;
+    }
+
+    for (auto &i: node[now]) { //현재 지점에서 갈 수 있는 노드.
+        if (V[i.first] == 1) //이미 방문한 경우
+            continue;
+
+        dfs(i.first, val + i.second);
+    }
+}
